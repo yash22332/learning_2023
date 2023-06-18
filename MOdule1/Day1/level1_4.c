7b0cc287ed17937318ebeba8133f1cea2df4d321
Yash Gupta <yashjnvk@gmail.comm>
2023-06-18 11:42:22 +0530

    Adding Solution 4

---
+++ MOdule1/Day1/level1_4.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2, result;
+    char operator;
+
+    
+    printf("Enter Operand 1: ");
+    scanf("%f", &num1);
+
+    
+    printf("Enter Operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    
+    printf("Enter Operand 2: ");
+    scanf("%f", &num2);
+
+    
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+            } 
+            else 
+            {
+                printf("Error: Division by zero is not allowed.\n");
+                return 1;
+            }
+            break;
+        default:
+            printf("Error: Invalid operator.\n");
+            return 1; 
+    }
+
+    
+    printf("Result: %.2f\n", result);
+
+    return 0;
+}
