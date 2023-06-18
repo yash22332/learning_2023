4adf726bd3611e6de6055d9b1835d0262482735c
Yash Gupta <yashjnvk@gmail.comm>
2023-06-18 12:35:56 +0530

    Solution 1

---
+++ MOdule1/Day3/level1_1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void printNumbers(int n) {
+    for (int i = 1; i <= n; i++) {
+        printf("%d", i);
+    }
+}
+
+void printSpaces(int n) {
+    for (int i = 1; i <= (2 * (n - 2)); i++) {
+        printf(" ");
+    }
+}
+
+void printPattern(int n) {
+    for (int i = n; i >= 1; i--) {
+        printNumbers(i);
+        printSpaces(i);
+        if (i > 1) {
+            printNumbers(i);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printPattern(n);
+
+    return 0;
+}
