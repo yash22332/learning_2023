a3750dfae76da8a341af04a128da39c5e5f54b3e
Yash Gupta <yashjnvk@gmail.comm>
2023-06-19 11:19:13 +0530

    Solution 2

---
+++ MOdule1/Day4/String_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int stringToInteger(const char *str) {
+    return atoi(str);
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    int num = stringToInteger(str);
+
+    printf("Converted integer: %d\n", num);
+
+    return 0;
+}
