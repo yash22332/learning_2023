08858a5751f926f586e884137cbab9bce03404e9
Yash Gupta <yashjnvk@gmail.comm>
2023-06-23 11:02:46 +0530

    Day5 Solution

---
+++ MOdule1/Day5/level1_4.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student {
+    char name[50];
+    int age;
+    float marks;
+};
+
+int main() {
+    int n;
+
+    printf("Enter the number of students: ");
+    scanf("%d", &n);
+
+    struct Student *students = (struct Student*)malloc(n * sizeof(struct Student));
+
+    if (students == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("\nEnter details for Student %d:\n", i + 1);
+
+        printf("Enter Name: ");
+        scanf("%s", students[i].name);
+
+        printf("Enter Age: ");
+        scanf("%d", &students[i].age);
+
+        printf("Enter Marks: ");
+        scanf("%f", &students[i].marks);
+    }
+
+    printf("\nStudent Details:\n");
+
+    for (int i = 0; i < n; i++) 
+    {
+        printf("\nStudent %d:\n", i + 1);
+        printf("Name: %s\n", students[i].name);
+        printf("Age: %d\n", students[i].age);
+        printf("Marks: %.2f\n", students[i].marks);
+    }
+
+    free(students);
+
+    return 0;
+}
